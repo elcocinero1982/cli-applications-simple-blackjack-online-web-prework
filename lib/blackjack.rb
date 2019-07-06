be01789e8f7f38b98cce72733e57ff51d03fd789
jose luis <cortezluisjose@gmail.com>
2019-07-06 04:15:15 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -34,12 +34,20 @@ end
 
 def hit?
   # code hit? here
+ describe "#display_card_total" do
+  it "accepts one argument, the card total" do
+    expect { display_card_total(7) }.to_not raise_error
+  end
  
+  it "prints the value of the cards to the screen" do
+    expect($stdout).to receive(:puts).with("Your cards add up to 8")
+    display_card_total(8)
+  end
 end
 
 def invalid_command
   # code invalid_command here
-  expect(self).to receive(:get_user_input).and_return("s")
+
 end
 
 #####################################################
